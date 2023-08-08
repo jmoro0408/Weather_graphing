d9b79aef8ff6c0c9b2fc03def382ac9f77a99fe6
James Moro <james.moro.11@gmail.com>
2023-08-08 20:31:38 +0100

    init commit

---
+++ ingest.py
@@ -0,0 +1,38 @@
+from pathlib import Path
+from typing import Union
+
+import requests
+
+urls = {
+    "tmax": r"https://www.metoffice.gov.uk/pub/data/weather/uk/climate/datasets/Tmax/ranked/UK.txt",
+    "tmin": r"https://www.metoffice.gov.uk/pub/data/weather/uk/climate/datasets/Tmin/ranked/UK.txt",
+    "tmean": r"https://www.metoffice.gov.uk/pub/data/weather/uk/climate/datasets/Tmean/ranked/UK.txt",
+    "sunshine": r"https://www.metoffice.gov.uk/pub/data/weather/uk/climate/datasets/Sunshine/ranked/UK.txt",
+    "rainfall": r"https://www.metoffice.gov.uk/pub/data/weather/uk/climate/datasets/Rainfall/ranked/UK.txt",
+}
+
+
+def grab_url_text_data(url: str, save_text_dir: Union[Path, str]) -> None:
+    """
+
+    Args:
+        url (str): url to grab text data from, should direct to a .txt html
+        e.g "http://www.gutenberg.org/files/11/11-0.txt"
+        save_text_dir (Union[Path, str]): directory to save text data in .
+
+    Returns:
+    """
+
+    response = requests.get(url)
+    response.raise_for_status()
+    response.encoding = "UTF-8"
+    with open(save_text_dir, "w", encoding="utf-8") as f:
+        f.write(response.text)
+    print(f"Success! URL {url} data successfully written to {save_text_dir}")
+    return None
+
+
+if __name__ == "__main__":
+    for key, url in urls.items():
+        SAVE_DIR = Path(Path.cwd(), "data", f"{key}.txt")
+        grab_url_text_data(url, SAVE_DIR)
